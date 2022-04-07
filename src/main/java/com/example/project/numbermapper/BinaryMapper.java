7ac43c371000d94a5671fef2814882f3d3e3dadd
Jan Recker <Jan.Recker@msg.group>
2022-04-07 16:21:29 +0200

    Cleanup

--- src/main/java/com/example/project/numbermapper/BinaryMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.numbermapper;
-
-public class BinaryMapper implements GenericNumberMapper {
-
-    public String map(int value) {
-        return BaseNumberMapper.mapToBase(value, 2);
-    }
-}
